f9270597d36f7aecf3cc5f82154e4376dbf0e945
Iska <ya@ti@>
2023-11-14 15:21:19 +0300

    hexlet fix 4

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -5,7 +5,7 @@ import { getName, logic } from '../src/cli.js';
 const name = getName();
 
 const task = (rightAnswers, name) => {
-    const progKolvoEl = 10;//*(max-min)+min
+    const progKolvoEl = Math.floor(Math.random()*5+5);//*(max-min)+min
     const progN = Math.floor(Math.random()*29+1);
     const firstEl = Math.floor(Math.random()*19+1);
     const arr = [];
@@ -16,7 +16,7 @@ const task = (rightAnswers, name) => {
     const censPos = Math.floor(Math.random()*(progKolvoEl-1)+1);
     const rightAnsw=arr[censPos];
     arr[censPos] = '..';
-    console.log(`Question ${arr.join(" ")}`);
+    console.log(`Question: ${arr.join(" ")}`);
     let userAnswer = readlineSync.question('Your answer: ');
     if(userAnswer==rightAnsw){
         console.log('Correct!');
